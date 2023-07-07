b2ca90c5060b8cc73c821e0970ae4fb0b669c48e
Caro Appleby <caroline.appleby@cantab.net>
2023-07-07 11:47:24 +0100

    Sanitize rendered HTML

--- src/components/Doc.svelte
+++ src/components/Doc.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
   export let doc_object;
+  import sanitizeHtml from "sanitize-html";
   import { marked } from "marked";
   marked.use({
-  mangle: false,
-  headerIds: false,
-  gfm: false,
-})
+    mangle: false,
+    headerIds: false,
+    gfm: false,
+  });
 </script>
 
 <p>{doc_object.description}</p>
 {#if doc_object.doc}
-  <!--TODO sanitize this markdown! -->
-  <p>{@html marked.parse(doc_object.doc)}</p>
+  <p>{@html sanitizeHtml(marked.parse(doc_object.doc))}</p>
 {/if}
